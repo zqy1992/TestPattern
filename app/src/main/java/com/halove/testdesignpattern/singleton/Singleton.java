a7cab8cdfef9eba102b80d25b5d4731436457053
zhuqingyun <zhuqingyun@halove.com>
2019-01-05 16:09:50 +0800

    7

--- app/src/main/java/com/halove/testdesignpattern/singleton/Singleton.java
+++ app/src/main/java/com/halove/testdesignpattern/singleton/Singleton.java
@@ -3,6 +3,7 @@ package com.halove.testdesignpattern.singleton;
 public class Singleton {
     private static Singleton mInstance = null;
     private static String s = "2";
+    private static String s3 = "23";
 
 
     public static Singleton getInstanceFromHolder(){
